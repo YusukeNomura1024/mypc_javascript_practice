c4f3f9c46964bcf8c50d91f87c32746709fae84e
YusukeNomura1024 <joblesser@gmail.com>
2022-01-04 00:18:34 +0900

    [Finish] reduce関数

--- 04_javascript/079_reduce関数/start/main.js
+++ 04_javascript/079_reduce関数/start/main.js
@@ -5,5 +5,15 @@ function tag(accu, curr) {
     return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+
+function reduce(arry, callback, defaultValue) {
+    let accu = defaultValue;
+    for(let i = 0; i < arry.length; i++) {
+        let curr = arry[i];
+        accu = callback(accu, curr);//accuに値が蓄積されるという機能もまだない
+        //コールバックの戻り値をaccuに再代入することで、蓄積の機能を持たせることができる
+    }
+    return accu;
+}
+const result = reduce(strArry, tag, "");
 console.log(result);
