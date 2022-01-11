28e2a8bc1b30cc6a801e69b779263ed8f89a4342
YusukeNomura1024 <joblesser@gmail.com>
2022-01-11 23:58:31 +0900

    [Start] thisとbindについて

--- 04_javascript/114_thisとオブジェクトについて/start/main.js
+++ 04_javascript/114_thisとオブジェクトについて/start/main.js
@@ -7,8 +7,24 @@ const obj = {
             console.log(this);
         };
         window.setTimeout(fn);  
+        //このsetTimeout関数はここで実行されるのではなく、
+        //別で定義されているwindowオブジェクトの中で実行されるので、
+        //setTimeout関数を使って呼び出した場合に、呼び出した関数内でthisがあれば
+        //そのthisはwindowを呼び出す
         //thisの示す値はどんどん階層を上がっていったときに直近となるオブジェクトである
-        
+        //window.setTimeout(function() {
+        //     console.log(this);
+        // })
+    }
+}
+
+//windowをオブジェクトとして考えると
+//以下のように定義されていることになり
+//setTimeout関数やその他のプロパティでthisを使うということは
+//windowオブジェクトを呼び出したことだとわかる
+const window = {
+    setTimeout: function(fn) {
+        fn();
     }
 }
 
