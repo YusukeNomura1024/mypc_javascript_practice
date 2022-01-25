9c446df531a0de78e6207324d28d43f70c0c70df
YusukeNomura1024 <joblesser@gmail.com>
2022-01-26 01:09:32 +0900

    [Finish] Swiperでスライダーを作成３

--- 05_javascript実践編/070_Swiperでスライダーアニメーション/start/main.js
+++ 05_javascript実践編/070_Swiperでスライダーアニメーション/start/main.js
@@ -1,7 +1,5 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  effect: 'coverflow',
-
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider(".swiper-container");
+  hero.start({delay: 2000});
 });
+
